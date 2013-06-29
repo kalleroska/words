63d431b7a8c914a62b28aaa626ff14551205ba0d
Kalle Saari <kasaar2@gmail.com>
2013-06-29 12:51:38 -0500

    Added further error checking

--- morphisms.py
+++ morphisms.py
@@ -8,19 +8,23 @@ def Iterate(h, a='0', N=10**4):
     - h is a morphism given as a dictionary.
     - a is the letter h is iterated on.
     """
+    if (not a in h.keys()) or (not h[a].startswith(a)):
+        print("Cannot iterate", h, "on", a)
+        return None
     aux = h[a]
     ind = 1
     while len(aux) < N:
         try:
             aux += h[aux[ind]]
         except:
-            print("The value", aux[ind], "is not in the domain of", h)
-            break
+            print(aux[ind], "is not in the domain of", h)
+            return None
         ind += 1
     return aux[:N]
 
 if __name__ == '__main__':
     h = {'0': '012', '1': '02', '2': '201'}
     print(Iterate(h, '2', 30))
+    print(Iterate(h, '3', 30))
     h['2'] = '203'
     print(Iterate(h, '2', 30))
