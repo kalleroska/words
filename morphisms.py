1e28e17555b4ac96fabd9064283d3d18b6c08f0e
Kalle Saari <kasaar2@gmail.com>
2013-06-29 12:45:11 -0500

    Added a check to ensure that the range is included in the domain of a morphism in Iterate.

--- morphisms.py
+++ morphisms.py
@@ -11,10 +11,16 @@ def Iterate(h, a='0', N=10**4):
     aux = h[a]
     ind = 1
     while len(aux) < N:
-        aux += h[aux[ind]]
+        try:
+            aux += h[aux[ind]]
+        except:
+            print("The value", aux[ind], "is not in the domain of", h)
+            break
         ind += 1
     return aux[:N]
 
 if __name__ == '__main__':
     h = {'0': '012', '1': '02', '2': '201'}
+    print(Iterate(h, '2', 30))
+    h['2'] = '203'
     print(Iterate(h, '2', 30))
