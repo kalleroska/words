05a4e11eb18540b9f4c2ba7ba0baa15f42955ea8
Kalle Saari <kasaar2@gmail.com>
2013-06-30 21:19:57 -0500

    Moved the try-except pair outside the while loop.

--- morphisms.py
+++ morphisms.py
@@ -5,19 +5,24 @@ def iterate(h, a='0', N=10**4):
 
     - h is a morphism given as a dictionary.
     - a is the letter h is iterated on.
+
+    Example:
+    >>> h = {'0': '012', '1': '02', '2': '201'}
+    >>> print(iterate(h, '2', 30))
+    201012020120220101220101202201
     """
     if (not a in h.keys()) or (not h[a].startswith(a)):
         print("Cannot iterate", h, "on", a)
         return None
     aux = h[a]
     ind = 1
-    while len(aux) < N:
-        try:
-            aux += h[aux[ind]]
-        except:
-            print(aux[ind], "is not in the domain of", h)
-            return None
-        ind += 1
+    try:
+        while len(aux) < N:
+                aux += h[aux[ind]]
+                ind += 1
+    except:
+        print(aux[ind], "is not in the domain of", h)
+        return None
     return aux[:N]
 
 if __name__ == '__main__':
